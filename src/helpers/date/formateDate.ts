9114f8cd1c533d353ffe376df35461be1b73ee0f
Nikita Sheremet <nikita.sheremet253@gmail.com>
2025-03-18 10:53:59 -0400

    update format for date cell in expense data

--- src/helpers/date/formateDate.ts
+++ src/helpers/date/formateDate.ts
@@ -1,5 +1,6 @@
 export enum DateFormat {
   YYYY_MM_DD = 'YYYY-MM-DD',
+  DD_MMMM_YYYY = 'DD-MMMM-YYYY',
 }
 export function formatDate(date: Date | string | number, format: DateFormat): string {
   const dateObject = new Date(date)
@@ -7,6 +8,12 @@ export function formatDate(date: Date | string | number, format: DateFormat): st
   switch (format) {
     case DateFormat.YYYY_MM_DD:
       return dateObject.toISOString().split('T')[0]
+    case DateFormat.DD_MMMM_YYYY:
+      return dateObject.toLocaleDateString('en-US', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+      })
     default:
       return dateObject.toString()
   }
